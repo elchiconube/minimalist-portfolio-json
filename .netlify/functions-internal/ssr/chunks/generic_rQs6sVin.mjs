90545c46cea783da9df90eaeb838656cbbd2a789
Oscar Bustos <oscarbustosgarrido@gmail.com>
2024-04-09 12:41:58 +0200

    improve

--- .netlify/functions-internal/ssr/chunks/generic_rQs6sVin.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_ecAmDtyc.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_2gABq-vq.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_wO6CVKZG.mjs');
-
-export { page };
